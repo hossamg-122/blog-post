4897384d98847e90ea89a6f0747a4881a2d1ea77
Hossam Gamal Ali <79874817+hossamg-122@users.noreply.github.com>
2022-06-26 00:59:48 +0200

    style: dark and light mode themes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React from 'react';
+import { useSelector } from 'react-redux'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import "./App.css";
 import { Home } from "./pages";
 
 function App() {
+  const {mode} =useSelector((state)=>state.blog)
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode
+        },
+      }),
+    [mode],
+  );
   return (
     <div>
+      <ThemeProvider theme={theme}>
       <Home />
+      </ThemeProvider>
+     
       
     </div>
   );
--- src/store/reducers/blogReducer.js
+++ src/store/reducers/blogReducer.js
@@ -1,5 +1,6 @@
 const initState = {
   posts: [],
+  mode:'light'
 };
 
 /**
