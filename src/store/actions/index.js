c4f03ae188afd24a7875598874a38f06b6edb6c8
Hossam Gamal Ali <79874817+hossamg-122@users.noreply.github.com>
2022-06-26 20:21:10 +0200

    feat: fetch users, posts and comments actions

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,14 +1,57 @@
 import { jsonplaceholder } from "../../api's/jsonPlaceHolder";
+import _ from "lodash";
 
-export const fetchPosts = () => {
+const _fetchUsers = _.memoize(async (userId) => {
+  try {
+    return await jsonplaceholder.get(`./users/${userId}`);
+  } catch (error) {
+    console.log(error, error.response);
+  }
+});
+export const fetchUser = async (userId) => {
+  return _fetchUsers(userId);
+};
+export const fetchPosts = (setLoading) => {
   return async (dispatch) => {
     try {
-      const {data} = await jsonplaceholder.get("/posts");
+      const { data } = await jsonplaceholder.get("/posts");
+      const postsWithUsers = await Promise.all(
+        data.map(async (post) => {
+          return await fetchUser(post.userId)
+            .then(({ data }) => {
+              return { ...post, user: { ...data } };
+            })
+            .catch((error) => {
+              console.log(error, error.response);
+            });
+        })
+      );
+
       dispatch({
-        type:'posts',
-        payload:data
-      })
-     
+        type: "posts",
+        payload: postsWithUsers,
+      });
+      setLoading(false);
+    } catch (error) {
+      console.log("error", error.response);
+    }
+  };
+};
+export const fetchComments = (postId, setLoadComments, setExpanded) => {
+  return async (dispatch, getState) => {
+    setLoadComments(true);
+    try {
+      const { data } = await jsonplaceholder.get(`/posts/${postId}/comments`);
+      const modifiedPosts = getState().blog.posts.map((post) => {
+        return post.id === postId ? { ...post, comments: data } : post;
+      });
+      dispatch({
+        type: "posts",
+        payload: modifiedPosts,
+      });
+
+      setLoadComments(false);
+      setExpanded(true);
     } catch (error) {
       console.log("error", error.response);
     }
