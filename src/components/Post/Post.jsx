f81cf6ec9ab0b0075bcee044d35aa9962733df84
Hossam Gamal Ali <79874817+hossamg-122@users.noreply.github.com>
2022-06-26 13:05:25 +0200

    feat: create post card

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,51 @@
+import { Box, Paper, IconButton, Avatar, Button } from "@mui/material";
+import React from "react";
+import { postBox, postAvatar, postButton } from "./Post.style";
+import { useTheme } from "@mui/material/styles";
+import { PostForm } from "../";
+
+export const Post = () => {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <Box component={Paper} sx={{ m: 2, p: 3, display: "flex" }}>
+        <IconButton sx={{ p: 0, mr: 1 }}>
+          <Avatar
+            alt="Hossam Gamal"
+            src={require("../../assets/profile.png")}
+            sizes="small"
+          />
+        </IconButton>
+        <Button
+          variant="text"
+          fullWidth
+          onClick={handleClickOpen}
+          sx={{
+            borderRadius: 20,
+            border: "2px solid #ccc",
+            color: theme.palette.mode === "dark" ? "white" : "gray",
+            display: "flex",
+            justifyContent: "flex-start",
+            pl: 3,
+            textTransform: "capitalize",
+          }}
+        >
+          Start a post
+        </Button>
+      </Box>
+      <PostForm
+        title="Create a post"
+        buttonText='Post'
+        open={open}
+        handleClose={handleClose}
+      />
+    </>
+  );
+};
