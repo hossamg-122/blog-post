24514377f1c020f743d1c630b9dccfd439eb2ffc
Hossam Gamal Ali <79874817+hossamg-122@users.noreply.github.com>
2022-06-26 20:19:43 +0200

    fix: formatting

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,47 +1,49 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import LoadingButton from '@mui/lab/LoadingButton';
-import CommentIcon from '@mui/icons-material/Comment';
-import { CreateComment } from '../';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import { IconButton, Typography } from "@mui/material";
+import { red } from "@mui/material/colors";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import LoadingButton from "@mui/lab/LoadingButton";
+import CommentIcon from "@mui/icons-material/Comment";
+import { CreateComment, CommentList } from "../";
+import { useDispatch } from "react-redux";
+import { fetchComments } from "../../store/actions";
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
   return <IconButton {...other} />;
 })(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
     duration: theme.transitions.duration.shortest,
   }),
 }));
 
-export const PostCard=({post})=> {
+export const PostCard = ({ post }) => {
+  const dispatcher = useDispatch();
   const [expanded, setExpanded] = React.useState(false);
-
+  const [loadComments, setLoadComments] = React.useState(false);
   const handleExpandClick = () => {
-    setExpanded(!expanded);
+    if (post?.comments?.length > 0) {
+      setExpanded(!expanded);
+      return;
+    }
+ dispatcher(fetchComments(post.id, setLoadComments, setExpanded));
   };
 
   return (
-    <Card sx={{m:2 }}>
+    <Card sx={{ m: 2 }}>
       <CardHeader
         avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe" >
-            R
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+          {post?.user?.name[0]}
           </Avatar>
         }
         action={
@@ -49,35 +51,33 @@ export const PostCard=({post})=> {
             <MoreVertIcon />
           </IconButton>
         }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
+        title={post?.user?.name}
+        subheader={post?.user?.email}
       />
-     
+
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-        {post.body}
+          {post.body}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-      <LoadingButton
+        <LoadingButton
           size="small"
           onClick={handleExpandClick}
           endIcon={<CommentIcon />}
-          loading={false}
+          loading={loadComments}
           loadingPosition="end"
           variant="outlined"
         >
           Comments
         </LoadingButton>
-        
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-            <CreateComment />
-          
-         
+          <CreateComment />
+          <CommentList comments={post.comments} />
         </CardContent>
       </Collapse>
     </Card>
   );
-}
+};
