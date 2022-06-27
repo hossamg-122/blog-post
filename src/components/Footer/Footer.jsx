9d3531ee97b464836ff8c9cdf1327c52753d0b2e
Hossam Gamal Ali <79874817+hossamg-122@users.noreply.github.com>
2022-06-27 21:30:24 +0200

    style: dark mode

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,18 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
-import { footerText } from "./Footer.style";
 
 export const Footer = () => {
   return (
-    <Box component="footer" sx={{ textAlign: "center",mt:'auto',py:8 }}>
+    <Box component="footer" sx={{ textAlign: "center", mt: "auto", py: 8 }}>
       <Typography
-        component="a"
         variant="body2"
-        sx={footerText}
+        color="text.primary"
+        sx={{
+          opacity: ".3",
+          cursor: "pointer",
+          transition: "all .25s ease-in-out",
+          ":hover": { opacity: 1 },
+        }}
       >
         Atomica, All rights reserved
       </Typography>
