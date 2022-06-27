cfe07b4848adb9bfe4e7614cb0a2691c2439bb42
Hossam Gamal Ali <79874817+hossamg-122@users.noreply.github.com>
2022-06-27 22:20:42 +0200

    style: floating navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,6 +7,8 @@ import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import Container from "@mui/material/Container";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import CssBaseline from "@mui/material/CssBaseline";
 
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
@@ -39,68 +41,91 @@ export const Navbar = () => {
     handleCloseUserMenu();
     dispatcher(logOut());
   };
-  return (
-    <AppBar  enableColorOnDark color="default">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Box sx={{ display: "flex", flexGrow: 1 }}>
-            <img
-              src={
-                theme.palette.mode === "dark"
-                  ? require("../../assets/logo-dark.png")
-                  : require("../../assets/logo-light.png")
-              }
-              alt="Atomica"
-              loading="lazy"
-              height={30}
-            />
-          </Box>
+  function ElevationScroll({children}) {
+    
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0,
+     
+    });
 
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}></Box>
-          <IconButton
-            sx={{ mr: 2 }}
-            onClick={handleThemeChange}
-            color="inherit"
-          >
-            {theme.palette.mode === "dark" ? (
-              <Brightness7Icon />
-            ) : (
-              <Brightness4Icon />
-            )}
-          </IconButton>
-          {isLogin ? (
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Profile />
-                </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: "45px" }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+  }
+
+  return (
+    <>
+      <CssBaseline />
+      <ElevationScroll>
+        <AppBar enableColorOnDark color="default">
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <Box sx={{ display: "flex", flexGrow: 1 }}>
+                <img
+                  src={
+                    theme.palette.mode === "dark"
+                      ? require("../../assets/logo-dark.png")
+                      : require("../../assets/logo-light.png")
+                  }
+                  alt="Atomica"
+                  loading="lazy"
+                  height={30}
+                />
+              </Box>
+
+              <Box
+                sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+              ></Box>
+              <IconButton
+                sx={{ mr: 2 }}
+                onClick={handleThemeChange}
+                color="inherit"
               >
-                {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleLogout}>
-                    <Typography textAlign="center">{setting}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-          ) : null}
-        </Toolbar>
-      </Container>
-    </AppBar>
+                {theme.palette.mode === "dark" ? (
+                  <Brightness7Icon />
+                ) : (
+                  <Brightness4Icon />
+                )}
+              </IconButton>
+              {isLogin ? (
+                <Box sx={{ flexGrow: 0 }}>
+                  <Tooltip title="Open settings">
+                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                      <Profile />
+                    </IconButton>
+                  </Tooltip>
+                  <Menu
+                    sx={{ mt: "45px" }}
+                    id="menu-appbar"
+                    anchorEl={anchorElUser}
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    open={Boolean(anchorElUser)}
+                    onClose={handleCloseUserMenu}
+                  >
+                    {settings.map((setting) => (
+                      <MenuItem key={setting} onClick={handleLogout}>
+                        <Typography textAlign="center">{setting}</Typography>
+                      </MenuItem>
+                    ))}
+                  </Menu>
+                </Box>
+              ) : null}
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </ElevationScroll>
+    </>
   );
 };
