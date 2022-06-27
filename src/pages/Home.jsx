337c9df582a8e6026432c21583f806c40593351d
Hossam Gamal Ali <79874817+hossamg-122@users.noreply.github.com>
2022-06-27 20:45:58 +0200

    refactor: adding router

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Box } from "@mui/material";
-
+import { Box, Container } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
-import { Post, PostCard, SkeletonLoader } from "../components";
+import { Post, PostCard, SkeletonLoader,Footer } from "../components";
 import { fetchPosts } from "../store/actions";
-
 export const Home = () => {
   const [loading, setLoading] = useState(true);
   const dispatcher = useDispatch();
@@ -13,13 +11,20 @@ export const Home = () => {
     dispatcher(fetchPosts(setLoading));
   }, []);
   return (
-    <Box component="main" sx={{}}>
-      <Post />
-      {loading ? (
-        <SkeletonLoader />
-      ) : (
-        posts?.map((post) => <PostCard key={post.id} post={post} />)
-      )}
-    </Box>
+    <Container
+      maxWidth="sm"
+      component="main"
+      sx={{ display: "flex", flexDirection: "column", height: "90vh" }}
+    >
+      <Box component="main" sx={{}}>
+        <Post />
+        {loading ? (
+          <SkeletonLoader />
+        ) : (
+          posts?.map((post) => <PostCard key={post.id} post={post} />)
+        )}
+      </Box>
+      <Footer />
+    </Container>
   );
 };
