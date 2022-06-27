4a1f48e71e589ba3d481804a6aa250fbf3cf71c6
Hossam Gamal Ali <79874817+hossamg-122@users.noreply.github.com>
2022-06-27 20:44:30 +0200

    fix: format

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -8,24 +8,32 @@ import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { IconButton } from "@mui/material";
-import "./comment.css"
+import "./comment.css";
+import { Profile } from "../Profile/Profile";
+import { EDIT_COMMENT } from "../../store/actions/types";
+import { deleteComment } from "../../store/actions";
+import { MoreIcon } from "../";
 export const CommentList = ({ comments = [] }) => {
   return (
     <List sx={{ width: "100%" }}>
       {comments.map((comment) => (
         <div key={comment.id}>
           <ListItem
-         
-           secondaryAction={
-<IconButton sx={{alignSelf:'flex-start'}} edge="end" aria-label="settings">
-                  <MoreVertIcon />
-                </IconButton>
-           }
-           
+            secondaryAction={
+              <MoreIcon
+                title="Post Actions"
+                action={EDIT_COMMENT}
+                element={comment}
+                deleteAction={deleteComment}
+              />
+            }
           >
-            
-            <ListItemAvatar sx={{alignSelf:'flex-start',mt:1 }}>
-              <Avatar alt="Remy Sharp">{comment?.email[0]}</Avatar>
+            <ListItemAvatar sx={{ alignSelf: "flex-start", mt: 1 }}>
+              {comment.email === "hossamg@atomica.ai" ? (
+                <Profile />
+              ) : (
+                <Avatar alt="Remy Sharp">{comment?.email[0]}</Avatar>
+              )}
             </ListItemAvatar>
             <ListItemText
               primary={comment.email}
